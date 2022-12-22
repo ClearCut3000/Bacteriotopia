a1767dd9a21dd2182b309fa0cd8b08b2543ca7d3
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-22 21:00:48 +0300

    Initial Commit

---
+++ Bacteriotopia/BacteriotopiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BacteriotopiaApp.swift
+//  Bacteriotopia
+//
+//  Created by Николай Никитин on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct BacteriotopiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
