ef1a58220fe1ae5fd517a575b07a61dac48e1ff6
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-22 21:26:00 +0300

    added Bacteria class

---
+++ Bacteriotopia/Bacteria.swift
@@ -0,0 +1,54 @@
+//
+//  Bacteria.swift
+//  Bacteriotopia
+//
+//  Created by Николай Никитин on 22.12.2022.
+//
+
+import SwiftUI
+
+class Bacteria {
+
+  //MARK: - Properties
+  enum Direction {
+    case north, east, west, south
+
+    var rotation: Double {
+      switch self {
+      case .north: return 0
+      case .east: return 90
+      case .south: return 180
+      case .west: return 270
+      }
+    }
+
+    var opposite: Direction {
+      switch self {
+      case .north: return .south
+      case .east: return .west
+      case .south: return .north
+      case .west: return .east
+      }
+    }
+
+    var next: Direction {
+      switch self {
+      case .north: return .east
+      case .east: return .south
+      case .south: return .west
+      case .west: return .north
+      }
+    }
+  }
+
+  var row: Int
+  var column: Int
+  var color = Color.gray
+  var directionn = Direction.north
+
+  //MARK: - Init
+  init(row: Int, column: Int) {
+    self.row = row
+    self.column = column
+  }
+}
--- Bacteriotopia/ContentView.swift
+++ Bacteriotopia/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+          HStack {
+            Text("Green: 0")
+            Spacer()
+            Text("Bacteriotopia")
+            Spacer()
+            Text("Red: 0")
+          }
+          .font(.system(size: 24).weight(.black))
+          VStack {
+            ForEach(0..<11, id: \.self) { row in
+              HStack {
+                ForEach(0..<22, id: \.self) { column in
+                  Text("X")
+                }
+              }
+            }
+          }
         }
         .padding()
+        .fixedSize()
+        .preferredColorScheme(.dark)
     }
 }
 
